e73819cbd73298028d94f072cf163a57e15f0043
youngtrashbag <yuule.smeker@gmail.com>
2020-12-08 12:01:51 +0100

    feat: add game of life and improve readme, for easy instructions
    - very fast ticks
    - easy instructions on how to create wasm package/project for rust

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 
 playing around with Web Assembly
 
+## How create packets
+
+1. `cargo generate --git https://github.com/rustwasm/wasm-pack-template`
+    `Project Name: new`
+2. `cd new`
+3. code away
+4. `wasm-pack build`
+5. `cargo generate --git https://github.com/rustwasm/create-wasm-app`
+    `Project Name: www`
+6. `npm init wasm-app www`
+7. `cd www`
+8. add this line to `www/package.json`
+
+    ```
+    "dependencies": {
+        "wasm-game-of-life": "file:../pkg"
+    },
+    ```
+
+    right above `devDependencies: { ... }`
+
+9. `npm install`
+10. `npm run start`
+11. enjoy
--- game-of-life/www/index.js
+++ game-of-life/www/index.js
@@ -1,3 +1,24 @@
-import * as wasm from "game-of-life";
+import { Universe } from "game-of-life";
 
-wasm.greet();
+// function to wait (because of tick)
+function holdOn( millisecondsToWait )
+{
+    setTimeout( function() {
+    /* do something or nothing; this waits for a minute because 60000 milliseconds have been passed */
+    }, millisecondsToWait );
+}
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+  // wait 1 sec
+  //holdOn(1000);
+
+  requestAnimationFrame(renderLoop);
+};
+
+//setTimeout( () => requestAnimationFrame(renderLoop), 1000);
+requestAnimationFrame(renderLoop);
